c1c9f8d7c882fa6d1a5c517d1b6c1c7461559909
Nathan Mayes <amayesingnathan123@gmail.com>
2023-03-11 22:29:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# timer-pool
+
+An hook-in timer pool library for timing applications.
+
+## Getting Started
+
+### Dependencies
+
+* Python3
+* premake5 - Will be downloaded and extracted as a binary when the setup script is run if it is not found.
+* Currently only supports Windows and Linux (Ubuntu tested).
+
+### Installing
+
+* Clone the repo and run `setup.bat` for Windows or `setup.sh` for Linux.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ premake5.lua
@@ -0,0 +1,30 @@
+workspace "timer-pool"
+
+    configurations 
+    { 
+        "Debug",
+        "Release"
+    }
+    
+    platforms
+    {
+        "x64",
+        "ARM32",
+        "ARM64"
+    }
+
+	filter "platforms:x64"
+		architecture "x86_64"
+
+	filter "platforms:ARM32"
+		architecture "ARM"
+
+ 	filter "configurations:ARM64"
+		architecture "ARM64"
+
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["timer-pool"] 	= "%{wks.location}/timer-pool/src"
+
+include "timer-pool"
